97be2f712abbb66bf9fe61c026584fcd3f72bf26
danzel-py <67574872+danzel-py@users.noreply.github.com>
2021-04-02 09:24:05 +0700

    Commit: Stock Price only
    can get stock price

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,42 @@
 'use strict';
 
-module.exports = function (app) {
+const fetch  = require('node-fetch');
+const { response } = require('../server');
+
+module.exports = function (app, collection) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+    .get(async function (req, res){
+      let stocks = await req.query.stock
+      if (typeof stocks === 'string'){
+        const resp1 = await fetch(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stocks}/quote`)
+        const stock1 = await resp1.json()
+        const stockData = {
+            stock: stock1.symbol,
+            price: stock1.latestPrice
+          }
+        res.send({stockData: stockData})
+      }
+      else if(stocks.length === 2){
+        if(stocks[1].length!== 4 || stocks[0].length!==4) return res.send({error: 'Invalid stocks'})
+        const resp1 = await fetch(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stocks[0]}/quote`)
+        const resp2 = await fetch(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stocks[1]}/quote`)
+        const stock1 = await resp1.json()
+        const stock2 = await resp2.json()
+        const stockData1 = {
+          stock: stock1.symbol,
+          price: stock1.latestPrice
+        }
+        const stockData2 = {
+          stock: stock2.symbol,
+          price: stock2.latestPrice
+        }
+        const stockData = [stockData1, stockData2]
+        res.send({stockData: stockData})
+      }
+      else{
+        res.send({error: 'Invalid stocks'})
+      }
     });
     
 };
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 'use strict';
 require('dotenv').config();
 const express     = require('express');
-const bodyParser  = require('body-parser');
 const cors        = require('cors');
 
 const apiRoutes         = require('./routes/api.js');
@@ -14,8 +13,29 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://new-user0:lol12345@learn-fcc.5st3o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("stockpricechecker").collection("users");
+  // perform actions on the collection object
+
+  //For FCC testing purposes
+  fccTestingRoutes(app);
+  
+  //Routing for API 
+  apiRoutes(app, collection);  
+      
+  //404 Not Found Middleware
+  app.use(function(req, res, next) {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
+  });
+});
 
 //Index page (static HTML)
 app.route('/')
@@ -23,18 +43,6 @@ app.route('/')
     res.sendFile(process.cwd() + '/views/index.html');
   });
 
-//For FCC testing purposes
-fccTestingRoutes(app);
-
-//Routing for API 
-apiRoutes(app);  
-    
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
-});
 
 //Start our server and tests!
 app.listen(process.env.PORT || 3000, function () {
